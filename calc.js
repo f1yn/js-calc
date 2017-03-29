27ba67f2f081c476a9f6dc3fa9d3d0c3a149bb95
Flynn Buckingham <flynnbuckingham@gmail.com>
2017-03-29 12:01:39 -0400

    Updated file to include MPL

--- calc.js
+++ calc.js
@@ -1,6 +1,9 @@
 /**
  * Created by flynnbuckingham on 2015-05-01.
- */
+ *
+ * /* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 function jsCalc(args){
     var self = this,
